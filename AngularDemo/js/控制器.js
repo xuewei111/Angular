f35ba9e5b14f827a93341185355bcec5856eda9c
xuewei111 <m13358109269_1@163.com>
2020-08-04 13:07:17 +0800

    Angular

---
+++ AngularDemo/js/控制器.js
@@ -0,0 +1,7 @@
+angular.module('myApp',[]).controller('namesCtrl',function($scope){
+    $scope.names = [
+        {name:'刘备',names:'玄德'},
+        {name:'关羽',names:'云长'},
+        {name:'张飞',names:'翼德'}
+    ]
+})
